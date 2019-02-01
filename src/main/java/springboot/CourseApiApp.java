f53ed37e85feae12c7e3ad8bd94576d452d15d41
Pavel Petr <pavel.petr@hotmail.com>
2019-02-01 15:02:31 +0100

    init project 2

--- src/main/java/springboot/CourseApiApp.java
+++ src/main/java/springboot/CourseApiApp.java
@@ -1,9 +1,18 @@
 package springboot;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Attention, all controllers services etc must be uder package of this app, otherwise will not be initialized
+ * @author pavel
+ *
+ */
+@SpringBootApplication
 public class CourseApiApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		SpringApplication.run(CourseApiApp.class, args);
 
 	}
 
