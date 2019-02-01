c6c8e9c1952f2970f1e80182328eb87aee8f4c3e
Pavel Petr <pavel.petr@hotmail.com>
2019-02-01 15:16:59 +0100

    derby db

--- src/main/java/springboot/topic/TopicService.java
+++ src/main/java/springboot/topic/TopicService.java
@@ -12,19 +12,15 @@ public class TopicService {
 
 	@Autowired
 	private TopicRepository topicRepository;
-	
-	private List<Topic> topics = Arrays.asList(new Topic("Spring", "Spring Framework", "Descripted spring"),
-			new Topic("Java", "Java Framework", "Descripted java"),
-			new Topic(".net", ".net Framework", "Descripted .net"));
 
 	public List<Topic> getAllTopics() {
-		List<Topic> topics = new ArrayList<>(); 
+		List<Topic> topics = new ArrayList<>();
 		topicRepository.findAll().forEach(topics::add);
 		return topics;
 	}
-	
+
 	public Topic getTopic(String id) {
-		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+		return topicRepository.findById(id).orElse(null);
 	}
 
 	public void addTopic(Topic topic) {
@@ -32,20 +28,10 @@ public class TopicService {
 	}
 
 	public void updateTopic(String id, Topic topic) {
-		for (int i = 0; i < topics.size(); i++) {
-			if (topics.get(i).getId().equals(id)) {
-				topics.set(i, topic);
-				break;
-			}
-		}
+		topicRepository.save(topic);
 	}
 
 	public void deleteTopic(String id) {
-		for (int i = 0; i < topics.size(); i++) {
-			if (topics.get(i).getId().equals(id)) {
-				topics.remove(i);
-				break;
-			}
-		}
+		topicRepository.delete(getTopic(id));
 	}
 }
